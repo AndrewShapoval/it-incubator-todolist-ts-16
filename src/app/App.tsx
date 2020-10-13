5c56524960e77dc1fddd0646e29e9fe08d278166
AndrewShapoval <andrewshapoval88@gmail.com>
2020-10-13 17:19:58 +0300

    add login and logout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,53 @@
-import React from 'react'
+import React, {useCallback, useEffect} from 'react'
 import './App.css'
-import {AppBar, Button, Container, IconButton, LinearProgress, Toolbar, Typography} from '@material-ui/core'
+import {
+    AppBar,
+    Button,
+    CircularProgress,
+    Container,
+    IconButton,
+    LinearProgress,
+    Toolbar,
+    Typography
+} from '@material-ui/core'
 import {Menu} from '@material-ui/icons'
 import {TodolistsList} from '../features/TodolistsList/TodolistsList'
 import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
-import {useSelector} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import {AppRootStateType} from './store'
-import {RequestStatusType} from './app-reducer'
+import {initializeAppTC, RequestStatusType} from './app-reducer'
 import {Route, Switch, Redirect} from 'react-router-dom'
 import {Login} from "../features/Login/Login";
+import {logoutTC} from "../features/Login/auth-reducer";
 
 type PropsType = {
     demo?: boolean
 }
 
 function App({demo = false}: PropsType) {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(initializeAppTC())
+    }, [])
+
     const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
+    const isInitialized = useSelector<AppRootStateType, boolean>((state) => state.app.isInitialized)
+    const isLoggedIn = useSelector<AppRootStateType, boolean>((state) => state.auth.isLoggedIn)
+
+
+    const logoutHandler = useCallback(() => {
+        dispatch(logoutTC())
+    }, [])
+
+    if (!isInitialized) {
+        return <div
+            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
+            <CircularProgress/>
+        </div>
+    }
+
     return (
         <div className="App">
             <ErrorSnackbar/>
@@ -27,7 +59,8 @@ function App({demo = false}: PropsType) {
                     <Typography variant="h6">
                         News
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    {isLoggedIn && <Button color="inherit" onClick={logoutHandler}>Log out</Button>}
+
                 </Toolbar>
                 {status === 'loading' && <LinearProgress/>}
             </AppBar>
--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import {Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, TextField, Button, Grid} from '@material-ui/core'
 import {useFormik} from "formik";
+import {loginTC} from "./auth-reducer";
+import {useDispatch, useSelector} from "react-redux";
+import {AppRootStateType} from "../../app/store";
+import { Redirect } from 'react-router-dom';
 
 export const Login = () => {
+
+    const dispatch = useDispatch()
+
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
+
+    type FormikErrorType = {
+        email?: string
+        password?: string
+        rememberMe?: boolean
+    }
 
     const formik = useFormik({
         initialValues: {
@@ -10,11 +24,28 @@ export const Login = () => {
             password: '',
             rememberMe: false
         },
+        validate: (values) => {
+            const errors: FormikErrorType = {};
+            if (!values.email) {
+                errors.email = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+            if (!values.password) {
+                errors.password = 'Required';
+            } else if (values.password.length < 6) {
+                errors.password = 'Must be 6 characters or less';
+            }
+            return errors;
+        },
         onSubmit: values => {
-            alert(JSON.stringify(values));
+            dispatch(loginTC(values))
         },
     })
 
+    if (isLoggedIn) {
+        return <Redirect to={"/"}/>
+    }
 
     return <Grid container justify="center">
         <Grid item xs={4}>
@@ -34,24 +65,20 @@ export const Login = () => {
                     <TextField
                         label="Email"
                         margin="normal"
-                        name="email"
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
+                        {...formik.getFieldProps("email")}
                     />
+                    {formik.errors.email ? <div style={{color: "red"}}>{formik.errors.email}</div> : null}
                     <TextField
                         type="password"
                         label="Password"
                         margin="normal"
-                        name="password"
-                        onChange={formik.handleChange}
-                        value={formik.values.password}
+                        {...formik.getFieldProps("password")}
                     />
+                    {formik.errors.password ? <div style={{color: "red"}}>{formik.errors.password}</div> : null}
                     <FormControlLabel
                         label={'Remember me'}
                         control={<Checkbox/>}
-                        name="rememberMe"
-                        onChange={formik.handleChange}
-                        checked={formik.values.rememberMe}
+                        {...formik.getFieldProps("rememberMe")}
                     />
                     <Button type={'submit'} variant={'contained'} color={'primary'}>Login</Button>
                 </FormGroup>
